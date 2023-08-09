cd613a46ce55acf6f8ae799cfd9c16b348355cae
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-08-09 19:28:25 +0900

    Create sendEmailAndRedirect.js

---
+++ functions/sendEmailAndRedirect.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer');
+
+exports.handler = async (event, context) => {
+  try {
+    const { email, redirectUrl } = JSON.parse(event.body);
+
+    // メール送信処理
+    const transporter = nodemailer.createTransport({
+      service: 'YourEmailServiceProvider', // 例: Gmailの場合 'gmail'
+      auth: {
+        user: 'your@email.com',
+        pass: 'your-password',
+      },
+    });
+
+    const mailOptions = {
+      from: 'your@email.com',
+      to: email,
+      subject: 'Location Information',
+      text: `Click the link to see your location: ${redirectUrl}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    // ファイルのURLを生成して返す
+    const fileUrl = `https://${event.headers.host}/data/${Date.now()}_location.html`;
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ fileUrl }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
+};
